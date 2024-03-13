d0e73fecabc0fe2fa993444ba5dd153193a28624
Nouman Jamil <noumanjamil113@gmail.com>
2024-03-13 14:42:17 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+let input1 = await inquirer.prompt({
+    name: "Num1",
+    type: "number",
+    message: "Enter Your First Number",
+});
+let input2 = await inquirer.prompt({
+    name: "Num2",
+    type: "number",
+    message: "Enter Your Second Number",
+});
+let totalSum = input1.Num1 + input2.Num2;
+console.log(totalSum);
